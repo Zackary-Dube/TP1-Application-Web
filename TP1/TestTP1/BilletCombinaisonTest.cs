13a5ec59b8233b01a7229dc6963978862ed1453c
2411136 <2411136@cegeplimoilou.ca>
2026-02-13 16:36:33 -0500

    Ajout des tests pour la classe BilletCombinaison.cs

---
+++ TP1/TestTP1/BilletCombinaisonTest.cs
@@ -0,0 +1,36 @@
+﻿using TP1;
+using NUnit.Framework;
+
+
+namespace TestTP1;
+
+[TestFixture]
+[TestOf(typeof(BilletCombinaison))]
+public class BilletCombinaisonTest
+{
+
+    [Test]
+    public void GenererNumerosDoitGenerer6NumerosUniqueEntre1Et50()
+    {
+        var billetCombinaison = new BilletCombinaison();
+        
+        billetCombinaison.GenererNumeros();
+
+        foreach (int numero in billetCombinaison.Numeros)
+        {
+            Assert.That(billetCombinaison.Numeros, Is.Unique);
+            Assert.That(numero, Is.LessThan(50));
+        }
+    }
+
+    [Test]
+    public void ComparerBilletsDoitRetournerNombreDeNumerosGagnant()
+    {
+        var BilletClient = new BilletCombinaison(new List<int>{1, 7, 9, 22, 15, 41});
+        var BilletTirage = new BilletCombinaison(new List<int>{4, 35, 9, 33, 15, 44});
+        
+        int resultat = BilletClient.ComparerBillets(BilletTirage, BilletClient);
+        
+        Assert.That(resultat, Is.EqualTo(2));
+    }
+}
