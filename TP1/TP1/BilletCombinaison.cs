f22f2fc0a77569cae23b0dc9a3a9cd52aee030a4
2411136 <2411136@cegeplimoilou.ca>
2026-02-13 15:50:42 -0500

    Ajout de la classe billet combinaion

---
+++ TP1/TP1/BilletCombinaison.cs
@@ -0,0 +1,46 @@
+﻿namespace TP1;
+
+public class BilletCombinaison
+{
+    private List<int> _numeros = new List<int>();
+    
+    private static Random random = new Random();
+    private static int numeroMax = 50;
+    
+    public List<int> Numeros { get => _numeros; set => _numeros = value; }
+
+    public BilletCombinaison()
+    {
+        
+    }
+
+    public BilletCombinaison(List<int> numeros)
+    {
+        Numeros = numeros;
+    }
+
+    public void GenererNumeros()
+    {
+        while (Numeros.Count < 6)
+        {
+            int numero = random.Next(1, numeroMax);
+            if (!Numeros.Contains(numero))
+            {
+                Numeros.Add(numero);
+            }
+        }
+    }
+
+    public int ComparerBillets(BilletCombinaison tirage, BilletCombinaison client)
+    {
+        int compteur = 0;
+        foreach (int numero in client.Numeros)
+        {
+            if (tirage.Numeros.Contains(numero))
+            {
+                compteur++;
+            }
+        }
+        return compteur;
+    }
+}
